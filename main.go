a8482d5b7a5419d365ba20b06738224a85d1f2b1
Dominik Froehlich <dominik.froehlich@sap.com>
2019-11-15 14:32:24 +0100

    Add godns flag

--- main.go
+++ main.go
@@ -9,8 +9,8 @@ import (
 )
 
 func main() {
-	var ns = flag.String("ns", "", "name server to use (default: use /etc/resolv.conf)")
-	var duration = flag.Duration("duration", 1*time.Minute, "how long do you want to query the name server?")
+	var godns = flag.Bool("godns", true, "use go dns resolver (true) or native getaddrinfo (false)")
+	var duration = flag.Duration("duration", 10*time.Second, "how long do you want to query the name server?")
 	var rate = flag.Int("rate", 1, "how many DNS queries per second?")
 
 	flag.Parse()
@@ -24,26 +24,31 @@ func main() {
 
 	var host = flag.Arg(0)
 
-	if *ns != "" {
-		//TODO
-	}
+	net.DefaultResolver.PreferGo = *godns
 
-	tickCount := 0
+	tickCount := int64(0)
+	tickTotal := int64(*duration) / int64(time.Second) * int64(*rate)
 	tick := time.Duration(float64(1) / float64(*rate) * float64(time.Second))
 	tickChan := time.Tick(tick)
 	endChan := time.After(*duration)
+	var tStart, tEnd time.Time
+	var tDuration time.Duration
+	var tDurationTotal time.Duration
 
 	for {
 		select {
 		case <-endChan:
 			fmt.Printf("\nExiting.\n")
+			fmt.Printf("Resolved %d/%d queries in %s. Actual rate was %.2f/s with an average query duration of %s\n",
+				tickCount, tickTotal, *duration, float64(tickCount)/float64(*duration/time.Second), tDurationTotal/time.Duration(tickCount))
 			os.Exit(0)
 		case <-tickChan:
 			tickCount++
-			tStart := time.Now()
+			tStart = time.Now()
 			addrs, err := net.LookupHost(host)
-			tEnd := time.Now()
-			tDuration := tEnd.Sub(tStart)
+			tEnd = time.Now()
+			tDuration = tEnd.Sub(tStart)
+			tDurationTotal += tDuration
 
 			if err != nil {
 				_, _ = fmt.Fprintf(os.Stderr, "%v  %v (count=%d, duration=%v)\n", tEnd.Format(time.RFC3339), err, tickCount, tDuration)
